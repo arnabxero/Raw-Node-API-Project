5e41f5fe50b5c40745489e0b379d2b033a892cf5
Iftekhar Ahmed Arnab <arnab.xero@gmail.com>
2023-03-01 17:05:23 +0600

    XXXXXX

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -13,7 +13,7 @@
 // Dependencies
 const { first } = require('lodash');
 const data = require('../../lib/data');
-const { hash } = require('../../helpers/utilities');
+const { hash, parseJSON } = require('../../helpers/utilities');
 
 const handler = {};
 
@@ -43,7 +43,7 @@ handler.users.post = (requestProperties, callback) => {
     const tosAgreement = typeof requestProperties.body.tosAgreement === 'boolean' && requestProperties.body.tosAgreement === true;
 
     if (firstName && lastName && phone && password && tosAgreement) {
-        data.read('', phone, (err1) => {
+        data.read('users', phone, (err1) => {
             if (err1) {
                 const userObject = {
                     firstName,
@@ -55,7 +55,7 @@ handler.users.post = (requestProperties, callback) => {
 
                 console.log(userObject);
 
-                data.create('', phone, userObject, (err2) => {
+                data.create('users', phone, userObject, (err2) => {
                     if (!err2) {
                         callback(200, {
                             message: 'User was created successfully!',
@@ -80,10 +80,82 @@ handler.users.post = (requestProperties, callback) => {
 };
 
 handler.users.get = (requestProperties, callback) => {
-    callback(200);
-};
+    const phone = typeof requestProperties.queryStringObject.phone === 'string' && requestProperties.queryStringObject.phone.trim().length === 11 ? requestProperties.queryStringObject.phone : false;
 
-handler.users.put = (requestProperties, callback) => {};
+    if (phone) {
+        data.read('users', phone, (err, U) => {
+            const user = { ...parseJSON(U) };
+
+            if (!err && user) {
+                delete user.password;
+                callback(200, user);
+            } else {
+                callback(404, {
+                    Message: 'User not found!',
+                });
+            }
+        });
+    } else {
+        callback(404, {
+            Message: 'No User Found!',
+        });
+    }
+};
+handler.users.put = (requestProperties, callback) => {
+    const firstName = typeof requestProperties.body.firstName === 'string' && requestProperties.body.firstName.trim().length > 0 ? requestProperties.body.firstName : false;
+
+    const lastName = typeof requestProperties.body.lastName === 'string' && requestProperties.body.lastName.trim().length > 0 ? requestProperties.body.lastName : false;
+
+    const phone = typeof requestProperties.body.phone === 'string' && requestProperties.body.phone.trim().length === 11 ? requestProperties.body.phone : false;
+
+    const password = typeof requestProperties.body.password === 'string' && requestProperties.body.password.trim().length > 0 ? requestProperties.body.password : false;
+
+    if (phone) {
+        if (firstName || lastName || password) {
+            data.read('users', phone, (err, uData) => {
+                const userData = JSON.parse(uData);
+
+                if (!err && userData) {
+                    if (firstName) {
+                        userData.firstName = firstName;
+                    }
+                    if (lastName) {
+                        userData.lastName = lastName;
+                    }
+                    if (password) {
+                        userData.password = hash(password);
+                    }
+
+                    console.log(userData);
+
+                    data.update('users', phone, userData, (err2) => {
+                        if (!err2) {
+                            callback(200, {
+                                Message: 'User Updated Successfully',
+                            });
+                        } else {
+                            callback(500, {
+                                Error: 'Server Error',
+                            });
+                        }
+                    });
+                } else {
+                    callback(400, {
+                        Error: 'Invalid Request',
+                    });
+                }
+            });
+        } else {
+            callback(400, {
+                Error: 'Invalid Phone Number 222',
+            });
+        }
+    } else {
+        callback(400, {
+            Error: 'Invalid phone number 111',
+        });
+    }
+};
 
 handler.users.delete = (requestProperties, callback) => {};
 
